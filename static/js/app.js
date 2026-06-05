076d0e93833859a5ae747c275463df489f946d0c
iskMil <iskramiluska@gmail.com>
2026-06-05 08:25:36 -0500

    Mejoras en login y sidebar

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Login cargado correctamente");
 });
+
+function toggleSidebar() {
+
+    const sidebar = document.querySelector('.sidebar');
+    const main = document.querySelector('.main');
+
+    sidebar.classList.toggle('collapsed');
+    main.classList.toggle('expanded');
+}
